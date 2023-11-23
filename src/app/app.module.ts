77fbde2438682a68d0f11da6642c06f7500997ce
saraththoonoli <thoonolisarath@gmail.com>
2023-11-23 18:00:10 +0530

    Film-blog

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
+import { WilsCardComponent } from './wils-card/wils-card.component';
+import { FooterComponent } from './footer/footer.component';
+import { HoverDirective } from './hover.directive';
+import { AboutComponent } from './about/about.component';
+import { BlogsComponent } from './blogs/blogs.component';
+import { TextColorDirective } from './text-color.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    WilsCardComponent,
+    FooterComponent,
+    HoverDirective,
+    AboutComponent,
+    BlogsComponent,
+    TextColorDirective
   ],
   imports: [
     BrowserModule,
